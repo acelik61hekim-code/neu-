76d04a1cfe9b18707897228def2cb1b19893f755
acelik61hekim-code <acelik61.hekim@gmail.com>
2026-07-24 05:06:49 +0200

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// Diese Datei nicht bearbeiten.
+// Weitere Informationen:
+// https://nextjs.org/docs/basic-features/typescript
